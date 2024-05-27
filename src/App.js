e5cc37dfb6dfac7046e3d00ec51898daa07d96de
mjkim <mjkim@ipartners.co.kr>
2024-05-28 00:49:19 +0900

    feat : main test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-      <Header /> 
+      <Header />
+      <Main />
     </div>
   );
 }
